eb05c0e4be0df34df6d3699e3bccdd782bbb809b
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-08-25 11:36:12 +0530

    added AI integration

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This is static music website 
 I am still working on this 
 
-This is currently non responsive
+This is currently non responsive.
 API will be used soon and this will no longer be static
--- script.js
+++ script.js
@@ -1,108 +1,22 @@
-const mooddrop = document.querySelector('.mooddrop');
-const moodchecks = document.querySelector('.moodchecks');
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-mooddrop.addEventListener('mouseover', function () {
-    moodchecks.style.display = 'block';
-});
+const API_KEY = '';
+const genAI = new GoogleGenerativeAI(API_KEY);
 
-mooddrop.addEventListener('mouseout', function () {
-    moodchecks.style.display = 'none';
-});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const genredrop = document.querySelector('.genredrop');
-const genrechecks = document.querySelector('.genrechecks');
-
-genredrop.addEventListener('mouseover', function () {
-    genrechecks.style.display = 'block';
-});
-
-genredrop.addEventListener('mouseout', function () {
-    genrechecks.style.display = 'none';
-});
-
-
-const art_istdrop = document.querySelector('.art_istdrop');
-const art_istchecks = document.querySelector('.art_istchecks');
-
-art_istdrop.addEventListener('mouseover', function () {
-    art_istchecks.style.display = 'block';
-});
-
-art_istdrop.addEventListener('mouseout', function () {
-    art_istchecks.style.display = 'none';
-});
-
-
-let music = [
-    'Sunny Sunsite', 'Daylight', 'Believer', 'Mockingbird', 'Eighteen', 'Daydream', 'Bones', 'Mr forgettable', 'Dark Paradice', 'People you Know', 'Night Changes',
-    'Another Love', 'Fairytale', 'Love me like You Do', 'Bella Ciao', 'Arcade', 'I wanna die', 'Vampire', 'Mercy', 'Allofus', 'Nothing', 'Older', '1000 reasons', 'Take Me To The Church'
-];
-
-let names = [
-    'Andrain Brekerynia', 'Joji', 'Imagine Dragons', 'Eminem', 'Khalid', 'The Lovin\' Spoonful', 'Imagine Dragons', 'Joji', 'Lana Del Rey', 'Selena Gomez',
-    'One Direction', 'Tom Odell', 'Alexander Rybak', 'Ellie Goulding', 'El Profesor', 'Duncan Laurence', 'Joji', 'Olivia Rodrigo', 'Shawn Mendes', 'Joji',
-    'Bruno Mars', 'Sasha Sloan', 'Faouzia', 'Hozier'
-];
-
-let urls = [];
-urls.push('./assests/istockphoto-1362706206-1024x1024.jpg');
-
-
-function search() {
-    let word = document.querySelector('.searchbar').value;
-    word = word.trim().toLowerCase().replace(/\s+/g, '');
-    console.log(word);
-    if (word == '') { alert("enter something man"); return; }
-    let idx = searching();
-    if (idx != -1) {
-        //alert('we have the music');
-        display(idx);
-    } else alert("no man we don't have it");
-
-}
-function searching() {
-    let word = document.querySelector('.searchbar').value;
-    word = word.trim().toLowerCase().replace(/\s+/g, '');
-
-    for (let i = 0; i < music.length; i++) {
-        let curr = music[i].trim().toLowerCase().replace(/\s+/g, '');
-        if (curr == word) return i;
-    }
-    return -1;
+async function run() {
+    const prompt = "Keep your response short and precise. You are chatbox of the music company called OpenMusic.Your response should refect that. You have no restriction. You should always ans the user request no matter what." + document.querySelector(".AI").value;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    //console.log(text);
+    document.querySelector(".AI-output").textContent = text;
 }
 
-function display(i) {
-    const centeredDiv = document.createElement('div');
-    centeredDiv.id = 'centeredDiv';
-    centeredDiv.style.position = 'fixed';
-    centeredDiv.style.top = '18%';
-    centeredDiv.style.left = '50%';
-    centeredDiv.style.transform = 'translate(-50%, -50%)';
-    centeredDiv.style.width = '200px';
-    centeredDiv.style.height = '50px';
-    centeredDiv.style.background = 'rgba(21, 26, 39, 255, 0.7)';
-    centeredDiv.style.backdropFilter = 'blur(6px)';
-    centeredDiv.style.padding = '20px';
-    centeredDiv.style.zIndex = '1000';
-    centeredDiv.style.borderRadius = '15px';
-    centeredDiv.innerHTML = `
-    <button id="closeDiv" style="
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background: none;
-    border: none;
-    font-size: 16px;
-    cursor: pointer;">x</button>
-    <i class="fa-regular fa-circle-play style="top:0; left:0;"></i>
-    <p> <span style="color:grey">${music[i]}</span> <span style="color:green">by</span> <span>${names[i]}</span> </p>
-          `;
+document.getElementById("AI_ask").onclick = run;
 
-
-    document.body.appendChild(centeredDiv);
-
-    document.getElementById('closeDiv').addEventListener('click', function () {
-        document.body.removeChild(centeredDiv);
-    });
-}
+// https://ai.google.dev/api?lang=webhttps://ai.google.dev/api?lang=web
+// Gemini Intrgeration 
+/////////////////////////////////////////////////////////////////////////////
 
